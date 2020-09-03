9bff3ab0e42704a81e600f6413eb68f1fa8a7c6c
serjG901 <70537823+serjG901@users.noreply.github.com>
2020-09-03 21:58:56 +0300

    Update todoArray.js

--- day03/todoArray.js
+++ day03/todoArray.js
@@ -1,8 +1,10 @@
-let todoArray = [];
+//определяем пустой массив для добавления в него объекты todo
+const todoArray = [];
 
-for (i = 0; i < 10; i++) {
+//сделаем массив из 10-ти объектов todo
+for (let i = 0; i < 10; i++) {
     let todo = {
-        todoId: i,
+        id: i,
         title: `titleTodo${i}`,
         text: `textTodo${i}`,
         complete: false
@@ -11,28 +13,40 @@ for (i = 0; i < 10; i++) {
 };
 
 let todoToAdd = {
-    todoId: 10,
+    id: 10,
     title: `HANDtitle10`,
     text: `HANDtext10`,
     complete: false
 };
 
 addTodo(todoToAdd);
-removeTodo(4);
+removeTodo(7);
 changeTodoTitle(2, 'NEWtitleForId2');
 changeTodoStatus(1, true);
 
-for (i = 0; i < todoArray.length; i++) {
-    switch (i) {
-        case 1: alert('todo with id 1 complete TRUE');
+todoArray.forEach((todo) => {
+    switch (todo.id) {
+        case 1: console.log('todo with id 1 complete TRUE');
             break;
-        case 4: alert('todo with id 4 have been delete');
+        case 2: console.log('todo with id 2 have been change title');
             break;
-        case 9: alert('todo with id 10 have been add handly');
+        case 8: console.log('todo with id 7 have been delete');
+            break;
+        case 10: console.log('todo with id 10 have been add handly');
             break;
     };
-    alert(JSON.stringify(todoArray[i]));
-};
+    let indexTodo = getIndexTodo(todo.id);
+    console.log(indexTodo);
+    console.log(JSON.stringify(todoArray[indexTodo], null, 4));
+});
+
+function getIndexTodo(todoId) {
+    let indexTodo = todoArray.findIndex((todo) => todo.id === todoId)
+    if (indexTodo === -1) {
+        throw new Error("Id not found!");
+    };
+    return indexTodo;
+}
 
 function addTodo(newTodo) {
     todoArray.push(newTodo);
@@ -40,16 +54,19 @@ function addTodo(newTodo) {
 };
 
 function removeTodo(todoId) {
-    todoArray.splice(todoId, 1);
+    let indexTodo = getIndexTodo(todoId);
+    todoArray.splice(indexTodo, 1);
     //return todoArray;
 };
 
 function changeTodoTitle(todoId, newTitle) {
-    todoArray[todoId].title = newTitle;
+    let indexTodo = getIndexTodo(todoId);
+    todoArray[indexTodo].title = newTitle;
     //return todoArray;
 };
 
 function changeTodoStatus(todoId, isComplete) {
-    todoArray[todoId].complete = isComplete;
+    let indexTodo = getIndexTodo(todoId);
+    todoArray[indexTodo].complete = isComplete;
     //return todoArray;
 };

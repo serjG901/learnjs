75e6c8c83bdea86afd2fcb25d15026e39cb0e5bc
serjG901 <70537823+serjG901@users.noreply.github.com>
2020-09-08 19:30:58 +0300

    update formatier

--- day04/test.js
+++ day04/test.js
@@ -1,80 +1,110 @@
 ﻿describe("makeTodos(n) - циклом создает массив объектов в количестве n", () => {
+    describe("проверим количество созданных элементов при n = 100", () => {
+        it(`n = 100 - длинна массива 100 элементов`, () =>
+            assert.equal(makeTodos(100).length, 100));
+    });
 
-  describe("проверим количество созданных элементов при n = 100", () => {
-    it(`n = 100 - длинна массива 100 элементов`, () => assert.equal(makeTodos(100).length, 100));
-  });
+    describe("проверим количество созданных элементов при n = 756", () => {
+        it(`n = 756 - длинна массива 756 элементов`, () =>
+            assert.equal(makeTodos(756).length, 756));
+    });
 
-  describe("проверим количество созданных элементов при n = 756", () => {
-    it(`n = 756 - длинна массива 756 элементов`, () => assert.equal(makeTodos(756).length, 756));
-  });
-
-  describe("проверим количество созданных элементов при n = 0", () => {
-    it(`n = 0 - длинна массива 0 элементов`, () => assert.equal(makeTodos(0).length, 0));
-  });
-  // ... другие тесты. Можно писать и describe, и it блоки.
+    describe("проверим количество созданных элементов при n = 0", () => {
+        it(`n = 0 - длинна массива 0 элементов`, () =>
+            assert.equal(makeTodos(0).length, 0));
+    });
+    // ... другие тесты. Можно писать и describe, и it блоки.
 });
 
 describe("addTodo = (todoArray, newTodo) добавляет объект в массив", () => {
+    describe("проверим добавление объекта в пустой массив", () => {
+        it(`addTodo( [], {} ).length == 1`, () =>
+            assert.equal(addTodo([], {}).length, 1));
+    });
 
-  describe("проверим добавление объекта в пустой массив", () => {
-    it(`addTodo( [], {} ).length == 1`, () => assert.equal(addTodo([], {}).length, 1));
-  });
-
-  describe("проверим добавление множества объектов в пустой массив", () => {
-    let newArray = (n) => {
-      let arr = [];
-      for (i = 0; i < n; i++) {
-        arr = addTodo(arr, {});
-      }
-      return arr;
-    }
-    let n = 100;
-    it(`newArray(${n}).length == ${n}`, () => assert.equal(newArray(n).length, n));
-  });
-  // ... другие тесты. Можно писать и describe, и it блоки.
+    describe("проверим добавление множества объектов в пустой массив", () => {
+        let newArray = (n) => {
+            let arr = [];
+            for (i = 0; i < n; i++) {
+                arr = addTodo(arr, {});
+            }
+            return arr;
+        };
+        let n = 100;
+        it(`newArray(${n}).length == ${n}`, () =>
+            assert.equal(newArray(n).length, n));
+    });
+    // ... другие тесты. Можно писать и describe, и it блоки.
 });
 
 describe("removeTodo = (todoArray, todoId) удаляет объект из массива", () => {
+    describe("проверим удаление объекта с id = 1 из массива [ { id: 1 } ]", () => {
+        it(`removeTodo( [ { id: 1 } ], 1 ).lenght == 0`, () =>
+            assert.equal(removeTodo([{ id: 1 }], 1).length, 0));
+    });
 
-  describe("проверим удаление объекта с id = 1 из массива [ { id: 1 } ]", () => {
-    it(`removeTodo( [ { id: 1 } ], 1 ).lenght == 0`, () => assert.equal(removeTodo([{ id: 1 }], 1).length, 0));
-  });
-
-  // ... другие тесты. Можно писать и describe, и it блоки.
+    // ... другие тесты. Можно писать и describe, и it блоки.
 });
 
 describe("changeTodoTitle = (todoArray, todoId, title) изменяет значение title в объекте", () => {
-
-  it(`проверим изменение значения свойства title`, () => {
-    assert.equal(changeTodoTitle([{ id: 1, title: "oldTitle" }], 1, "newTitle")[0].title, "newTitle");
-  })
-  // ... другие тесты. Можно писать и describe, и it блоки.
+    it(`проверим изменение значения свойства title`, () => {
+        assert.equal(
+            changeTodoTitle([{ id: 1, title: "oldTitle" }], 1, "newTitle")[0]
+                .title,
+            "newTitle"
+        );
+    });
+    // ... другие тесты. Можно писать и describe, и it блоки.
 });
 
 describe("changeTodoComplete = (todoArray, todoId, complete) изменяет значение complete в объекте", () => {
+    it(`проверим изменение свойства complete c false на true`, () => {
+        assert.equal(
+            changeTodoComplete([{ id: 1, complete: false }], 1, true)[0]
+                .complete,
+            true
+        );
+    });
 
-  it(`проверим изменение свойства complete c false на true`, () => {
-    assert.equal(changeTodoComplete([{ id: 1, complete: false }], 1, true)[0].complete, true);
-  })
-
-  it(`проверим изменение свойства complete c true на false`, () => {
-    assert.equal(changeTodoComplete([{ id: 1, complete: true }], 1, false)[0].complete, false);
-  })
-  // ... другие тесты. Можно писать и describe, и it блоки.
+    it(`проверим изменение свойства complete c true на false`, () => {
+        assert.equal(
+            changeTodoComplete([{ id: 1, complete: true }], 1, false)[0]
+                .complete,
+            false
+        );
+    });
+    // ... другие тесты. Можно писать и describe, и it блоки.
 });
 
 describe("editedTodo = (todoArray, todoEdited) изменяет объект в массиве по его todoEdited.id", () => {
+    it(`изменим значение двух свойств и проверим изменение значения 1-го свойства`, () => {
+        assert.equal(
+            editedTodo(
+                [{ id: 1, property1: "oldValue1", property2: "oldValue2" }],
+                { id: 1, property1: "newValue1", property2: "newValue2" }
+            )[0].property1,
+            "newValue1"
+        );
+    });
 
-  it(`изменим значение двух свойств и проверим изменение значения 1-го свойства`, () => {
-    assert.equal(editedTodo([{ id: 1, property1: "oldValue1", property2: "oldValue2" }], { id: 1, property1: "newValue1", property2: "newValue2" })[0].property1, "newValue1");
-  })
+    it(`изменим значение двух свойств и проверим изменение значения 2-го свойства`, () => {
+        assert.equal(
+            editedTodo(
+                [{ id: 1, property1: "oldValue1", property2: "oldValue2" }],
+                { id: 1, property1: "newValue1", property2: "newValue2" }
+            )[0].property2,
+            "newValue2"
+        );
+    });
 
-  it(`изменим значение двух свойств и проверим изменение значения 2-го свойства`, () => {
-    assert.equal(editedTodo([{ id: 1, property1: "oldValue1", property2: "oldValue2" }], { id: 1, property1: "newValue1", property2: "newValue2" })[0].property2, "newValue2");
-  })
-
-  it(`изменим значение одного свойства и проверим чтобы не изменилось значение второго свойства`, () => {
-    assert.equal(editedTodo([{ id: 1, property1: "oldValue1", property2: "oldValue2" }], { id: 1, property1: "newValue1" })[0].property2, "oldValue2");
-  })
-  // ... другие тесты. Можно писать и describe, и it блоки.
+    it(`изменим значение одного свойства и проверим чтобы не изменилось значение второго свойства`, () => {
+        assert.equal(
+            editedTodo(
+                [{ id: 1, property1: "oldValue1", property2: "oldValue2" }],
+                { id: 1, property1: "newValue1" }
+            )[0].property2,
+            "oldValue2"
+        );
+    });
+    // ... другие тесты. Можно писать и describe, и it блоки.
 });
--- day04/todoTask.js
+++ day04/todoTask.js
@@ -1,7 +1,7 @@
 ﻿// ------------------------global varibals--------------------
 let globalID = 0;
 // -------------------------functions---------------------------
-const getId = () => globalID += 1;
+const getId = () => (globalID += 1);
 
 const makeTodo = (title, text, complete) => {
     return {
@@ -9,8 +9,8 @@ const makeTodo = (title, text, complete) => {
         title,
         text,
         complete,
-    }
-}
+    };
+};
 
 const makeTodos = (n) => {
     let todos = [];
@@ -22,26 +22,36 @@ const makeTodos = (n) => {
         todos = addTodo(todos, todo);
     }
     return todos;
-}
+};
 
 const addTodo = (todoArray, newTodo) => todoArray.concat(newTodo);
 
-const removeTodo = (todoArray, todoId) => todoArray.filter(todoItem => todoItem.id !== todoId);
+const removeTodo = (todoArray, todoId) =>
+    todoArray.filter((todoItem) => todoItem.id !== todoId);
 
-const removeCompleted = todoArray => todoArray.filter(todoItem => !todoItem.complete);
+const removeCompleted = (todoArray) =>
+    todoArray.filter((todoItem) => !todoItem.complete);
 
 const changeTodoTitle = (todoArray, todoId, title) => {
-    return todoArray.map(todoItem => (todoItem.id === todoId) ? { ...todoItem, title } : todoItem);
-}
+    return todoArray.map((todoItem) =>
+        todoItem.id === todoId ? { ...todoItem, title } : todoItem
+    );
+};
 
 const changeTodoComplete = (todoArray, todoId, complete) => {
-    return todoArray.map(todoItem => (todoItem.id === todoId) ? { ...todoItem, complete } : todoItem);
-}
+    return todoArray.map((todoItem) =>
+        todoItem.id === todoId ? { ...todoItem, complete } : todoItem
+    );
+};
 
 const editedTodo = (todoArray, todoEdited) => {
     if (todoEdited) {
-        return todoArray.map(todoItem => (todoItem.id === todoEdited.id) ? { ...todoItem, ...todoEdited } : todoItem);
+        return todoArray.map((todoItem) =>
+            todoItem.id === todoEdited.id
+                ? { ...todoItem, ...todoEdited }
+                : todoItem
+        );
     } else {
         throw new Error("Entering edited object not correct or null");
     }
-}
+};

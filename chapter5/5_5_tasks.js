d31ff9ff2050c34769b31e973bcfc1a00318ca95
serjG901 <70537823+serjG901@users.noreply.github.com>
2020-09-17 22:53:57 +0300

    Update 5_5_tasks.js

--- chapter5/5_5_tasks.js
+++ chapter5/5_5_tasks.js
@@ -1,1 +1,19 @@
-﻿
+﻿//task 1
+console.log("task 1------");
+
+const ucFirst = (str) => {
+    if (str) return str[0].toUpperCase() + str.slice(1, str.length);
+    return str;
+};
+
+const camelize = (str) => {
+    return str
+        .split("-")
+        .map((word, index) => (index ? ucFirst(word) : word))
+        .join("");
+};
+console.log(camelize("list-style-image"));
+console.log(camelize("background-color"));
+
+// task 2
+console.log("task 2-------");

80d52b1b8c11cb72cee6e3640642600fddd6ab4d
serjG901 <70537823+serjG901@users.noreply.github.com>
2020-09-30 14:47:07 +0300

    add 6.8 tasks

---
+++ chapter6/6_8_tasks.js
@@ -0,0 +1,33 @@
+﻿//task 1
+console.log("task 1 -------");
+
+const printNumbersInterval = (from, to) => {
+    let i = from;
+    const intervalID = setInterval(() => {
+        console.log("printNumbersInterval");
+        console.log(i++);
+        if (i > to) clearInterval(intervalID);
+    }, 1000);
+};
+printNumbersInterval(5, 10);
+
+const printNumbersTimeout = (from, to) => {
+    let i = from;
+    setTimeout(function run() {
+        console.log("printNumbersTimeout");
+        console.log(i);
+        if (i < to) setTimeout(run, 1000);
+        i++;
+    }, 1000);
+};
+printNumbersTimeout(1, 4);
+
+//task 2
+console.log("task 2 -------");
+let i = 0;
+
+setTimeout(() => console.log(i), 100);
+
+for (let j = 0; j < 100000000; j++) {
+    i++;
+}
